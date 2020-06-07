a3615a5cfc61ff57902301b97b9b46412f71f6b2
Dabeet660 <66428574+Dabeet660@users.noreply.github.com>
2020-06-07 10:40:33 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,109 +1,154 @@
-var ball, ballimg,paddleimg,paddle;
+var trex, trex_running, trex_collided;
+var ground, invisibleGround, groundImage;
+var CloudGroup , cloudIamge ;
+var ObstacleGroup, ObstacleImage1, ObstacleImage2, ObstacleImage3, ObstacleImage4, ObstacleImage5, ObstacleImage6;
 var gameState;
-var gameOver , gameRestart, overImg, restartImg;
-var count = 0;
-function preload() {
-  
-    ballimg = loadImage("ball.png");
-    paddleimg = loadImage("paddle.png");
-    overImg = loadImage("gameOver.png");
-    restartImg = loadImage("restart.png");
-}
-function setup() {
-  createCanvas(400, 400);
-   ball = createSprite(200,200,10,10);
-   ball.addImage(ballimg);
-   ball.velocityX = 9;   
+var count = 0; 
+var gameOver, gameRestart, gameOverImage, gameRestartImage;
 
-   paddle = createSprite(100,100,10,10);
-   paddle.addImage(paddleimg);
-   
-   gameOver = createSprite(200,200,10,10);
-   gameOver.addImage(overImg);
-   gameOver.scale = 0.4;
-   gameOver.visible = false;
+function preload(){
+  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
+  trex_collided = loadImage("trex_collided.png");
   
-   restart = createSprite(350, 200, 10,10);
-   restart.addImage(restartImg);
-   restart.scale = 0.4;
-   restart.visible = false;
+  groundImage = loadImage("ground2.png");
+  cloudImage = loadImage("cloud.png");
+  ObstacleImage1 = loadImage("obstacle1.png");
+  ObstacleImage2 = loadImage("obstacle2.png");
+  ObstacleImage3 = loadImage("obstacle3.png");
+  ObstacleImage4 = loadImage("obstacle4.png");
+  ObstacleImage5 = loadImage("obstacle5.png");
+  ObstacleImage6 = loadImage("obstacle6.png");
+
+  gameOverImage = loadImage("gameOver.png");
+  gameRestartImage = loadImage("restart.png");
+}
+
+function setup() {
+  createCanvas(1920, 1080);
   
-   gameState = "Play";
+  trex = createSprite(50,380,20,50);
+  trex.addAnimation("running", trex_running);
+  trex.scale = 0.5;
   
-  /* give the ball an initial velocity of 9 in the X direction */
   
+  gameState = "Play";
+  
+  ground = createSprite(200,380,400,20);
+  ground.addImage("ground",groundImage);
+  ground.x = ground.width /2;
+  ground.velocityX = -2;
+  
+  invisibleGround = createSprite(200,390,400,10);
+  invisibleGround.visible = false;
+
+  CloudGroup = new Group();
+  ObstacleGroup = new Group();
+
+  gameOver = createSprite(200,200,10,10);
+  gameOver.addImage(gameOverImage);
+  gameOver.visible = false;
+  gameOver.scale = 0.4;
+  
+  gameRestart = createSprite(350, 200, 10,10);
+  gameRestart.addImage(gameRestartImage);
+  gameRestart.visible = false;
+  gameRestart.scale = 0.4;
 
 }
 
 function draw() {
-  background(205,153,0);
+  background(255);
   
-   
-  edges = createEdgeSprites();
+   trex.collide(invisibleGround);
+
   if(gameState == "Play"){
+  if(keyDown("space")) {
+    trex.velocityY = -10;
+  }
+  
+  trex.velocityY = trex.velocityY + 0.8
+  
+  if (ground.x < 0){
+    ground.x = ground.width/2;
+  }
+  
+  count =  count +  Math.round((World.frameRate) / 60);
+  
+  SpawnClouds();
+  SpawnObs();
+  
+
+  if(ObstacleGroup.isTouching(trex)){
+      gameState = "Finish";
+      }
+  }
+
+  if(gameState == "Finish"){
+     count = 0;
+     trex.velocityY = 0;
+     ground.velocityX = 0;
+     gameOver.visible = true;
+     gameRestart.visible = true;
+     CloudGroup.setVelocityEach(0);
+     ObstacleGroup.setVelocityEach(0);
     
-  ball.bounceOff(edges[3]);
-  ball.bounceOff(edges[1]);
-  ball.bounceOff(edges[2]);
-  paddle.bounceOff(edges[0]);
-  paddle.bounceOff(edges[1]);
-  paddle.bounceOff(edges[2]);
-  paddle.bounceOff(edges[3]);
-  
-  if(keyDown(UP_ARROW))
-  {
-     paddle.velocityY = -3;
+    if(mousePressedOver(gameRestart)){
+       restartGame()
+     }
   }
-  
-  if(keyDown(DOWN_ARROW))
-  {
-    paddle.velocityY = 3;
-  }
-  
-  if(ball.bounceOff(paddle)){
-    randomVelocity();
-  }  
-    
-       count = count  + Math.round(World.frameRate / 60);
-      
-  }
-  
-  
-  if(ball.x < 0){
-     gameState = "End";
-   }
-  
-  if(gameState == "End"){
-    count = 0;
-    restart.visible = true;
-    gameOver.visible = true;
-    ball.velocityX = 0;
-    ball.x = 200;
-    ball.y = 200;
-    paddle.velocityX = 0;
-   }
-  
-  if(mousePressedOver(restart)){
-     restartGame();
-   }
-  
-  text("Score:" + count, 30,30);
-  text("FPS:" + Math.round(World.frameRate),50,50);
   drawSprites();
+  text("Score:" + count, 300,100);
+  
   
 }
 
-function randomVelocity()
-{
-  var gen_random_velocity = random(1,20);
-  ball.velocityX = 5;
+
+function SpawnObs(){
+  if(frameCount%90 == 0){
+     var obstacles = createSprite(600,360,10,10);
+     var randomNumber = Math.round(random(1,6));
+     switch(randomNumber){
+       case 1:
+         obstacles.addImage(ObstacleImage1);
+       break;
+       case 2:
+         obstacles.addImage(ObstacleImage2);
+       break;
+       case 3:
+         obstacles.addImage(ObstacleImage3);
+       break;
+       case 4:
+         obstacles.addImage(ObstacleImage4);
+       break;
+       case 5:
+         obstacles.addImage(ObstacleImage5);
+       break;
+       case 6:
+         obstacles.addImage(ObstacleImage6);
+       break;
+      }
+    obstacles.scale = 0.7;
+    obstacles.velocityX = -6;
+    obstacles.lieftime = 100;
+    ObstacleGroup.add(obstacles);
+    
+   }
 }
 
+function SpawnClouds(){
+   if(frameCount%60 == 0){
+    var cloud = createSprite(600,Math.round(random(0,350)),10,10);
+    cloud.addImage(cloudImage);
+    cloud.velocityX = -3;
+    CloudGroup.add(cloud);
+    cloud.lifetime = 200;
+    }
+}
 
 function restartGame(){
   gameState = "Play";
   gameOver.visible = false;
-  restart.visible = false;
-  ball.velocityX = 9;
-  
- }
+  gameRestart.visible = false;
+  CloudGroup.setVelocityXEach(-3);
+  ObstacleGroup.setVelocityXEach(-6);
+  }
